ef8cfb89b00a7320db10031e124258fbe96a38d3
Emmanuel1-code <126092577+Emmanuel1-code@users.noreply.github.com>
2023-04-27 19:14:55 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Navbar2 from "./Components/Navbar2";
+
+function App() {
+  return (
+    <>
+      <Navbar2 />
+      <Navbar />
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Gmail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Gmail() {
+  return (
+    <div>Gmail</div>
+  )
+}
+
+export default Gmail
